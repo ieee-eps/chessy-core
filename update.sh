c64fca64207e5340c5fa3ffe7cc92e49da2e4e7e
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-12-13 17:19:59 +0100

    Mise à jour de l'update.sh

--- update.sh
+++ update.sh
@@ -2,20 +2,18 @@
 
 source scripts/colors.sh
 
-remote="https://github.com/ieee-eps/chessy-system"
+remote="https://github.com/ieee-eps/chessy-core"
 
 function echom() {
   echo -e "\033[0m$*\033[0m"
 }
-
-echom "${BG_BRIGHT_BLACK} D ${COLOR_RESET}${FG_WHITE} Pull ..."
 
 version_local=$(git rev-parse HEAD)
 version_remote=$(git ls-remote "$remote" HEAD)
 
 version_remote=${version_remote:0:40}
 
-if [ "$version_remote" != "$version_local" ]; then
+if [ "$version_remote" == "$version_local" ]; then
   echom "${BG_BRIGHT_GREEN}${FG_BRIGHT_WHITE} K ${COLOR_RESET}${FG_BRIGHT_WHITE} À jour ! (${FG_BRIGHT_CYAN}${version_local:0:7}${FG_BRIGHT_WHITE})"
 else
   echom "${BG_BRIGHT_BLUE}${FG_BRIGHT_WHITE} I ${COLOR_RESET}${FG_BRIGHT_WHITE} Une nouvelle version a été trouvée !"
