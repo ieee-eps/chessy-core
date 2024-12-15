2d14545b27f7920bcc3118534c7fdea3b84748c3
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-12-15 19:25:26 +0100

    Update - Test boutons

--- interface/src/index.js
+++ interface/src/index.js
@@ -1,5 +1,44 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('node:path');
+const Gpio = require("onoff").Gpio;
+
+const PIN_OFFSET = 512;
+
+const pin_btn_1 = new Gpio(PIN_OFFSET+26, "in", "both");
+const pin_btn_2 = new Gpio(PIN_OFFSET+19, "in", "both");
+const pin_btn_3 = new Gpio(PIN_OFFSET+13, "in", "both");
+const pin_btn_4 = new Gpio(PIN_OFFSET+6, "in", "both");
+const pin_btn_5 = new Gpio(PIN_OFFSET+5, "in", "both");
+
+const btn = [
+  pin_btn_1,
+  pin_btn_2,
+  pin_btn_3,
+  pin_btn_4,
+  pin_btn_5
+]
+
+function unwatchAll() {
+  btn.forEach(v => {
+    v.unwatch();
+  })
+}
+
+function awaitButton() {
+  return new Promise((resolve, reject) => {
+    let delayer = null;
+    btn.forEach((b, i) => {
+      b.watch((_, v) => {
+        if(v===1) return;
+        if(delayer) clearTimeout(delayer);
+        delayer = setTimeout(() => {
+          unwatchAll();
+          resolve(i+1)
+        }, 100);
+      })
+    })
+  })
+}
 
 const createWindow = () => {
   const mainWindow = new BrowserWindow({
@@ -9,9 +48,7 @@ const createWindow = () => {
     titleBarStyle: "hidden",
     autoHideMenuBar: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false
+      preload: path.join(__dirname, 'preload.js')
     },
   });
 
@@ -20,6 +57,10 @@ const createWindow = () => {
 
 app.whenReady().then(() => {
   createWindow();
+
+  ipcMain.on("awaitButton", async () => {
+    ipcMain.emit("awaitButtonResponse", await awaitButton());
+  })
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
--- interface/src/preload.js
+++ interface/src/preload.js
@@ -1,2 +1,14 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+const { contextBridge, ipcRenderer  } = require('electron')
+
+contextBridge.exposeInMainWorld('chessy', {
+    awaitButton: () => function() {
+        return new Promise((resolve, reject) => {
+            ipcRenderer.emit("awaitButton")
+
+            ipcRenderer.once("awaitButtonResponse", (data) => {
+                resolve(data)
+            })
+        })
+    }
+})
+
