9601c4c18faab903d5f229c5bbca25bcb839533d
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-12-15 10:48:00 +0100

    Premier test Electron

--- interface/src/index.js
+++ interface/src/index.js
@@ -5,6 +5,9 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false,
+    titleBarStyle: "hidden",
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
