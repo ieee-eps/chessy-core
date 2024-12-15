f8d92c7768e085a187c1d99dab6bb5443cc8d89c
Mohamed-Yassin ROUIS <myassin.rouis@gmail.com>
2024-12-15 11:27:06 +0100

    Update

--- interface/start_interface.sh
+++ interface/start_interface.sh
@@ -1,4 +1,8 @@
-startx
-export DISPLAY=:0
-xhost +
-npx electron ./interface/src/index.js
+
+function run_s() {
+  export DISPLAY=:0
+  xhost +
+  npx electron ./interface/src/index.js
+}
+
+startx & run_s
